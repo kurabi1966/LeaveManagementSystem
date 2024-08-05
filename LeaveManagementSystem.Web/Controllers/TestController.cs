7209fcf5cf80093ad80749f39e4fa5c64d561b72
Mohammed Ammar Kurabi <kurabi@gmail.com>
2024-08-05 21:18:35 +0400

    first commit

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LeaveManagementSystem.Web.Models;
+
+namespace LeaveManagementSystem.Web.Controllers;
+
+public class TestController : Controller
+{
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var model = new TestViewModel { }; // we are ignoring the name
+        return View(model);
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace LeaveManagementSystem.Web.Models;
+
+public class TestViewModel
+{
+    public string? Name { get; set; }
+
+}
